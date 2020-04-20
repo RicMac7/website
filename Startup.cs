bd67d62faa5b3ea73261302dced2097cd9985cc6
RicMac7 <63457483+RicMac7@users.noreply.github.com>
2020-04-20 13:01:08 +0100

    Add files via upload

--- Startup.cs
+++ Startup.cs
@@ -1,15 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using CursoIEFP2._0.Data;
 
 namespace CursoIEFP2._0
 {
@@ -28,7 +21,7 @@ namespace CursoIEFP2._0
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
